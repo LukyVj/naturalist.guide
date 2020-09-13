e24514fe061b9a1ef92da01e8777fb12aad7b354
lukyvj <lucas.bonomi@gmail.com>
2020-09-13 20:16:42 +0200

    update misc

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import { Home, Search, Info, Map, MessageCircle } from "react-feather";
 
 const style = {
   root: css``,
@@ -11,25 +13,52 @@ const style = {
   `,
 };
 
-const menu = {
-  Home: "/",
-  Search: "/search",
-  About: "/about",
-  "MadamNazar.io": "https://madamnazar.io",
-  Discord: "https://discord.gg/RJ2YYN",
-};
+const menu = [
+  {
+    name: "Home",
+    url: "/",
+    icon: <Home />,
+  },
+  {
+    name: "Search",
+    url: "/search",
+    icon: <Search />,
+  },
+  {
+    name: "About",
+    url: "/about",
+    icon: <Info />,
+  },
+  {
+    name: "MadamNazar.io",
+    url: "https://madamnazar.io",
+    icon: <Map />,
+  },
+  {
+    name: "Discord Server",
+    url: "https://discord.gg/RJ2YYN",
+    icon: <MessageCircle />,
+  },
+];
 
-const linkClasses = "td-none color-white fw-bold";
+const linkClasses =
+  "td-none color-white fw-bold d-flex ai-center p-16 w-100p h-100p pos-relative";
 
 const Nav = () => {
   return (
-    <nav className="pos-sticky top-0 w-100p h-80 z-max ph-32 d-flex ai-center">
-      <ul className="lis-none p-0 m-0 w-100p">
+    <nav className="pos-sticky top-0 w-100p h-80 z-max d-flex ai-center">
+      <ul className="lis-none p-0 m-0 w-100p ph-32 d-flex ai-center jc-center">
         {Object.entries(menu).map((obj: any) => {
-          const key = obj[0];
-          const value = obj[1];
+          const name = obj[1].name;
+          const value = obj[1].url;
+          const icon = obj[1].icon;
+
+          console.log(obj);
           return (
-            <li className="d-inline-block p-16 bgc-black" css={style.item}>
+            <li
+              className="d-inline-block mh-8 bgc-black cursor-pointer"
+              css={style.item}
+            >
               {value.startsWith("http") ? (
                 <a
                   href={value}
@@ -37,12 +66,19 @@ const Nav = () => {
                   rel="noopener noreferrer"
                   className={linkClasses}
                 >
-                  {key}
+                  <span>{icon}</span>
+                  <span className="ml-8">{name}</span>
                 </a>
               ) : (
-                <Link to={value} className={linkClasses}>
-                  {key}
-                </Link>
+                <NavLink
+                  to={value}
+                  className={linkClasses}
+                  activeClassName="color-black bgc-farwest"
+                  exact
+                >
+                  <span>{icon}</span>
+                  <span className="ml-8">{name}</span>
+                </NavLink>
               )}
             </li>
           );
